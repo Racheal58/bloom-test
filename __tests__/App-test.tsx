016191910d7a7182f8f124040a8883d221b17730
Racheal Orukele <orukele_racheal@yahoo.com>
2022-03-19 13:09:51 +0100

    fix type errors & test logout button show when required

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,11 +9,16 @@ import {fireEvent, render, waitFor} from '@testing-library/react-native';
 import api from '../api';
 
 jest.mock('../api');
-api.login.mockResolvedValue({token: 'TOKEN'});
-api.logout.mockResolvedValue();
+(api.login as jest.MockedFunction<typeof api.login>).mockResolvedValue({
+  token: 'TOKEN',
+});
+(api.logout as jest.MockedFunction<typeof api.logout>).mockResolvedValue(
+  undefined,
+);
 
 it('navigates to page with log out button after entering pin', async () => {
   const {getByTestId, queryByTestId} = render(<App />);
+  expect(queryByTestId('log-out-btn')).toBeFalsy();
   const button0 = getByTestId('input-0');
   fireEvent.press(button0);
   fireEvent.press(button0);
